cce1fd415ecb59b61e04832e078e48f8bb56ecfa
Shawn Webster <shawn@shawnwebster.com>
2015-07-08 19:10:41 +1000

    adding prime numbers task

---
+++ Playgrounds/PrimeNumbers.playground/Pages/README.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*
+
+Task 1: Create a function to determine whether a given number is prime: (Int) -> (Bool)
+Note: A prime number is an integer greater than 1 that is only evenly divisible by 1 and itself.
+
+Task 2: Find the first 1000 prime numbers. Is there any way for you to optimise your solution?
+
+*/
